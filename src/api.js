d87524e5923da07c06e442489cd474eed1b485e4
Alexander <alexander.davidson87@gmail.com>
2021-10-31 14:46:04 -0500

    Update api.js

--- src/api.js
+++ src/api.js
@@ -16,20 +16,10 @@ const removeQuery = () => {
   }
 };
 
-export const checkToken = async (accessToken) => {
-  const result = await fetch(
-    `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
-  )
-    .then((res) => res.json())
-    .catch((error) => error.json());
-
-  return result;
-};
-
 const getToken = async (code) => {
   const encodeCode = encodeURIComponent(code);
   const { access_token } = await fetch(
-    `https://8456f66169.execute-api.eu-central-1.amazonaws.com/dev/api/token/${encodeCode}`
+    `https://8456f66169.execute-api.eu-central-1.amazonaws.com/dev/api/token` + '/' + encodeCode
   )
     .then((res) => {
       return res.json();
@@ -65,6 +55,16 @@ export const getAccessToken = async () => {
   }
   return accessToken;
 }
+
+export const checkToken = async (accessToken) => {
+  const result = await fetch(
+    `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
+  )
+    .then((res) => res.json())
+    .catch((error) => error.json());
+
+  return result;
+};
 
 export const getEvents = async () => {
   NProgress.start();
