5cabcd0f78c822d51ce85d9aa8a702b7c1a5cc25
Alexander <alexander.davidson87@gmail.com>
2021-10-31 13:09:00 -0500

    Update api.js

--- src/api.js
+++ src/api.js
@@ -17,24 +17,16 @@ const removeQuery = () => {
 };
 
 export const checkToken = async (accessToken) => {
-  const result = await fetch(
-    `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
-  )
-    .then((res) => res.json())
-    .catch((error) => error.json());
+  const result = await fetch(`https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`).then((res) => res.json()).catch((error) => error.json());
 
   return result;
 };
 
 const getToken = async (code) => {
   const encodeCode = encodeURIComponent(code);
-  const { access_token } = await fetch(
-    'https://8456f66169.execute-api.eu-central-1.amazonaws.com/dev/api/token' + '/' + encodeCode
-  )
-    .then((res) => {
-      return res.json();
-    })
-    .catch((error) => error);
+  const { access_token } = await fetch('https://8456f66169.execute-api.eu-central-1.amazonaws.com/dev/api/token' + '/' + encodeCode).then((res) => {
+    return res.json();
+  }).catch((error) => error);
 
   access_token && localStorage.setItem("access_token", access_token);
 
